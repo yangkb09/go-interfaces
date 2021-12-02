906084d567c3bc4d134e75527ed1b5619ab7ef2d
yangkb09 <yangkb09@gmail.com>
2021-12-02 13:43:49 -0500

    Sec 6: Interfaces in Practice

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+type englishBot struct{}
+type spanishBot struct{}
+
+func main() {
+	eb := englishBot{}
+	sb := spanishBot{}
+
+	printGreeting(eb)
+	printGreeting(sb)
+}
+
+func printGreeting(eb englishBot) {
+	fmt.Println(eb.getGreeting())
+}
+
+func printGreeting(sb spanishBot) {
+	fmt.Println(eb.getGreeting())
+}
+
+func (englishBot) getGreeting() string {
+	// VERY custom logic for generating an english greeting
+	return "Hi There!"
+}
+
+func (spanishBot) getGreeting() string {
+	// VERY custom logic for generating an spanish greeting
+	return "Hola!"
+}
